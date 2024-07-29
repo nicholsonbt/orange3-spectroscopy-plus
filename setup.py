15b761b30c85ff3ed6b80400baa105a79bbf892a
Ben Nicholson <86907890+nicholsonbt@users.noreply.github.com>
2024-07-29 22:44:11 +0100

    Update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 NAME = "Orange3-spectroscopy-plus"
 
-VERSION = "0.0.1"
+VERSION = "0.1.0"
 
 DESCRIPTION = "Add-on that extends the orange-spectroscopy"
 LONG_DESCRIPTION = open(path.join(path.dirname(__file__), 'README.pypi'),
